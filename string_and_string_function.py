11fa03638b9627af34532c1aec8f4f3228fb127f
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-07-28 23:52:12 +0530

    Create string_and_string_function.py

---
+++ string_and_string_function.py
@@ -0,0 +1,32 @@
+s="KavinMayil is studenT"
+print(s)
+print(type(s))
+print(s.upper())
+print(s.lower())
+print(s.capitalize())
+print(s.title())
+print(s.count("a"))
+print(s.endswith("nT"))
+print(s.find("a"))
+print(s.find("a",2))
+print(s.replace("a",'@'))
+a="KAVI2N"
+print(a.isupper())
+print(a.islower())
+print(a.isalnum())
+print(a.isalpha())
+s="he\nis\ngood"
+print(s)
+print(s.splitlines())
+print(s.splitlines(True))
+a="kavin mayil is a student"
+print(a.split(" "))
+a="kavin,mayil,is,a,student"
+print(a.split(','))
+s="     kavinmayil      "
+print(len(s))
+print(len(s.strip()))
+print(len(s.lstrip()))
+print(len(s.rstrip()))
+s='12-03-2025'
+print(s.partition('-'))
