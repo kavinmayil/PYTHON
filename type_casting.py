45488b8ea4a6d31db3db6e3315bf4b4532e688ee
KAVINKUMAR M <168561418+kavinmayil@users.noreply.github.com>
2025-07-28 22:06:57 +0530

    Create type_casting.py

---
+++ type_casting.py
@@ -0,0 +1,12 @@
+a=10.0
+print(a)
+print(type(a))
+b=int(a)
+print(b)
+print(type(b))
+
+
+a=int(input("Enter the value of A:"))
+b=int(input("Enter the value of B:"))
+c=a+b
+print("Total:",str(c))
